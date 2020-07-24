8374fe2a69531bf79c5f2aff7c340dd127da2468
yangxin <1393003255@qq.com>
2020-07-24 10:11:27 +0800

    feat: 替换空格

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/foroffer/easy/ReplaceSpace.java
@@ -0,0 +1,18 @@
+package org.yangxin.datastructurealgorithm.leetcode.foroffer.easy;
+
+/**
+ * 替换空格
+ *
+ * @author yangxin
+ * 2020/07/24 10:09
+ */
+public class ReplaceSpace {
+
+    public static String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We are happy."));
+    }
+}
