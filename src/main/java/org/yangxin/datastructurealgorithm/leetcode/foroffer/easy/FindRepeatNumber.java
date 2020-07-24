059470989a593c1a8dff03ace5224ad8be75cbed
yangxin <1393003255@qq.com>
2020-07-24 10:00:30 +0800

    feat: 数组中重复的数字

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/foroffer/easy/FindRepeatNumber.java
@@ -0,0 +1,30 @@
+package org.yangxin.datastructurealgorithm.leetcode.foroffer.easy;
+
+import java.util.Arrays;
+
+/**
+ * 数组中重复的数字
+ *
+ * @author yangxin
+ * 2020/07/24 09:54
+ */
+public class FindRepeatNumber {
+
+    public static int findRepeatNumber(int[] nums) {
+        // 先升序
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        System.out.println(findRepeatNumber(nums));
+    }
+}
