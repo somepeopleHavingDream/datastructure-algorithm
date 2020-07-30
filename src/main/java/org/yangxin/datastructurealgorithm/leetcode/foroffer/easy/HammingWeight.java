a841a1162d0075b6cb0dd0a955fc5f06a77afa2b
yangxin <1393003255@qq.com>
2020-07-30 16:36:29 +0800

    feat：二进制中1的个数

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/foroffer/easy/HammingWeight.java
@@ -0,0 +1,21 @@
+package org.yangxin.datastructurealgorithm.leetcode.foroffer.easy;
+
+/**
+ * 二进制中1的个数
+ *
+ * @author yangxin
+ * 2020/07/30 16:31
+ */
+public class HammingWeight {
+
+    /**
+     * you need to treat n as an unsigned value
+     */
+    public static int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(9));
+    }
+}
