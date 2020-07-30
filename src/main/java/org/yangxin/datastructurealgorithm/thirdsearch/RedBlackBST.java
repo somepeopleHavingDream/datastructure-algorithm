c26addb6a5fd1cb11553d3e083ff091c01c368dd
yangxin <1393003255@qq.com>
2020-07-30 18:02:27 +0800

    feat：读至红黑树的左右旋转

--- src/main/java/org/yangxin/datastructurealgorithm/thirdsearch/RedBlackBST.java
+++ src/main/java/org/yangxin/datastructurealgorithm/thirdsearch/RedBlackBST.java
@@ -1,6 +1,11 @@
 package org.yangxin.datastructurealgorithm.thirdsearch;
 
 /**
+ * 红黑树的一种等价的定义
+ * 红链接均为左链接
+ * 没有任何一个结点同时和两条红链接相连
+ * 该树是完美黑色平衡的，即任意空链接到根节点的路径上的黑链接数量相同
+ *
  * @author yangxin
  * 2020/07/24 11:53
  */
@@ -54,5 +59,35 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
 
         return x.color == RED;
+    }
+
+    /**
+     * 左旋转，它对应的方法接受一条指向红黑树中的某个结点的链接作为参数。
+     * 假设被指向的结点的右链接是红色的，这个方法会对树进行必要的调整并返回一个指向包含同一组键的子树并且其左链接为红色的根节点的链接
+     */
+    Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    private int size(Node x) {
+        return x == null ? 0 : x.N;
     }
 }
