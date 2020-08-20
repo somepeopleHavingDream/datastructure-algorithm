f50034462d11769c1e835d3e1776e3d3e5f5ad33
yangxin <1393003255@qq.com>
2020-08-20 16:33:01 +0800

    feat：对称的二叉树

--- src/main/java/org/yangxin/datastructurealgorithm/leetcode/foroffer/easy/IsSymmetric.java
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/foroffer/easy/IsSymmetric.java
@@ -11,23 +11,49 @@ import javax.swing.tree.TreeNode;
 public class IsSymmetric {
 
     public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left != null && right != null && left.val == right.val) {
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+
         return false;
     }
 
     public static void main(String[] args) {
+//        TreeNode node1 = new TreeNode(1);
+//        TreeNode node2 = new TreeNode(2);
+//        TreeNode node3 = new TreeNode(2);
+//        TreeNode node4 = new TreeNode(3);
+//        TreeNode node5 = new TreeNode(4);
+//        TreeNode node6 = new TreeNode(4);
+//        TreeNode node7 = new TreeNode(3);
+//        node1.left = node2;
+//        node1.right = node3;
+//        node2.left = node4;
+//        node2.right = node5;
+//        node3.left = node6;
+//        node3.right = node7;
+
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
         TreeNode node3 = new TreeNode(2);
         TreeNode node4 = new TreeNode(3);
-        TreeNode node5 = new TreeNode(4);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(3);
         node1.left = node2;
         node1.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node3.left = node6;
-        node3.right = node7;
+        node2.right = node4;
+        node3.right = node5;
 
         System.out.println(isSymmetric(node1));
     }
