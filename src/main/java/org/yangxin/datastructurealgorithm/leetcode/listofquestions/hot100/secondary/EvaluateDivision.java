296ac9fe510f47a720927487ab092601bc793c35
yangxin <1393003255@qq.com>
2022-02-27 18:11:34 +0800

    feat: EvaluateDivision

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/EvaluateDivision.java
@@ -0,0 +1,12 @@
+package org.yangxin.datastructurealgorithm.leetcode.listofquestions.hot100.secondary;
+
+/**
+ * @author yangxin
+ * 2022/2/27 18:02
+ */
+public class EvaluateDivision {
+
+    public static void main(String[] args) {
+        // 放弃
+    }
+}
