70cb72a2b9ed61e3e33d6678881d4fba6830dfff
yangxin <1393003255@qq.com>
2022-02-27 17:52:25 +0800

    feat: HouseRobberIII

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/HouseRobberIII.java
@@ -0,0 +1,70 @@
+package org.yangxin.datastructurealgorithm.leetcode.listofquestions.hot100.secondary;
+
+/**
+ * @author yangxin
+ * 2022/2/27 17:03
+ */
+@SuppressWarnings("AlibabaClassNamingShouldBeCamel")
+public class HouseRobberIII {
+
+    public static void main(String[] args) {
+//        TreeNode node1 = new TreeNode(3);
+//        TreeNode node2 = new TreeNode(2);
+//        TreeNode node3 = new TreeNode(3);
+//        TreeNode node4 = new TreeNode(3);
+//        TreeNode node5 = new TreeNode(1);
+//        node1.left = node2;
+//        node1.right = node3;
+//        node2.right = node4;
+//        node3.right = node5;
+
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(1);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.right = node6;
+
+        System.out.println(robt(node1));
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static int robt(TreeNode root) {
+        int[] result = dp(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    private static int[] dp(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
+
+        int[] result = new int[2];
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = root.val + left[0] + right[0];
+
+        return result;
+    }
+}
