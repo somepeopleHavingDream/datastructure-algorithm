0d5c9781d7747c48ee1af7392e2a622068a2b005
yangxin <1393003255@qq.com>
2022-02-27 18:30:10 +0800

    feat: TaskScheduler

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/TaskScheduler.java
@@ -0,0 +1,18 @@
+package org.yangxin.datastructurealgorithm.leetcode.listofquestions.hot100.secondary;
+
+/**
+ * @author yangxin
+ * 2022/2/27 18:12
+ */
+public class TaskScheduler {
+
+    public static void main(String[] args) {
+        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+        int n = 2;
+        System.out.println(leastInterval(tasks, n));
+    }
+
+    private static int leastInterval(char[] tasks, int n) {
+        return 0;
+    }
+}
