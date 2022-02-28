43a3b495b89c8e22665b5617f94218db42aeae15
yangxin <1393003255@qq.com>
2022-02-28 21:18:12 +0800

    feat: TaskScheduler

--- src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/TaskScheduler.java
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/TaskScheduler.java
@@ -1,18 +1,49 @@
 package org.yangxin.datastructurealgorithm.leetcode.listofquestions.hot100.secondary;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author yangxin
  * 2022/2/27 18:12
  */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
 public class TaskScheduler {
 
     public static void main(String[] args) {
-        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+//        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+//        int n = 2;
+
+//        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+//        int n = 0;
+
+        char[] tasks = {'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
         int n = 2;
+
         System.out.println(leastInterval(tasks, n));
     }
 
     private static int leastInterval(char[] tasks, int n) {
-        return 0;
+        // 统计每个任务出现的频数
+        int[] count = new int[26];
+        for (char task : tasks) {
+            count[task - 'A']++;
+        }
+
+        // 找到任务最多的频数
+        Arrays.sort(count);
+
+        // 计算出频数最大的任务数
+        int num = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (count[i] != count[25]) {
+                break;
+            }
+
+            num++;
+        }
+
+        return Math.max((count[25] - 1) * (n + 1) + num, tasks.length);
     }
 }
