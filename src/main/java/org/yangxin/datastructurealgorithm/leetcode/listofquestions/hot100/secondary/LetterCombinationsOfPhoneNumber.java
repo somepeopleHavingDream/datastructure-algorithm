cc46b0a50abf028e929445384d80ac16af0645aa
yangxin <1393003255@qq.com>
2022-03-01 22:34:55 +0800

    feat: LetterCombinationsOfPhoneNumber

---
+++ src/main/java/org/yangxin/datastructurealgorithm/leetcode/listofquestions/hot100/secondary/LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,21 @@
+package org.yangxin.datastructurealgorithm.leetcode.listofquestions.hot100.secondary;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author yangxin
+ * 2022/2/28 21:19
+ */
+public class LetterCombinationsOfPhoneNumber {
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+        System.out.println(letterCombinations(""));
+        System.out.println(letterCombinations("2"));
+    }
+
+    private static List<String> letterCombinations(String digits) {
+        return Collections.emptyList();
+    }
+}
