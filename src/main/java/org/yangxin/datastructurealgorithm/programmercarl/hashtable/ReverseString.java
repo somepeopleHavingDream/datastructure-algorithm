c1669ececa500face6f31651a4aab064cc8a4f7c
yangxin <1393003255@qq.com>
2022-03-13 20:48:43 +0800

    feat: ReverseString

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/hashtable/ReverseString.java
@@ -0,0 +1,24 @@
+package org.yangxin.datastructurealgorithm.programmercarl.hashtable;
+
+/**
+ * @author yangxin
+ * 2022/3/13 20:40
+ */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
+public class ReverseString {
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+//        char[] s = {'H', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(s);
+        System.out.println(s);
+    }
+
+    private static void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+}
