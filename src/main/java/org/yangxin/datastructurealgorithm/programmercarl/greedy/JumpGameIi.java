8e52ebb9a11aadd8a2ed673654ab09b57bb70048
yangxin <1393003255@qq.com>
2022-04-11 15:38:20 +0800

    feat: JumpGameIi

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/greedy/JumpGameIi.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/greedy/JumpGameIi.java
@@ -7,21 +7,38 @@ package org.yangxin.datastructurealgorithm.programmercarl.greedy;
 public class JumpGameIi {
 
     public static void main(String[] args) {
-//        int[] nums = {2, 3, 1, 1, 4};
-        int[] nums = {3, 2, 1, 0, 4};
+        int[] nums = {2, 3, 1, 1, 4};
+//        int[] nums = {2, 3, 0, 1, 4};
+//        int[] nums = {2, 1};
+//        int[] nums = {1, 2, 1, 1, 1};
+
         System.out.println(jump(nums));
     }
 
     private static int jump(int[] nums) {
         int result = 0;
 
-        int nextMaxStep = 0;
-        int currentStep = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            nextMaxStep = Math.max(i + nums[i], nextMaxStep);
-            if (i == currentStep) {
-                result++;
-                currentStep = nextMaxStep;
+        // 下一步的最远位置
+        int nextMaxIndex = 0;
+        // 当前步的最远位置
+        int currentMaxIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 计算下一步的最远位置
+            nextMaxIndex = Math.max(i + nums[i], nextMaxIndex);
+
+            if (i != currentMaxIndex) {
+                continue;
+            }
+
+            // 如果已经走到了当前步的最远距离
+            if (i == nums.length - 1) {
+                return result;
+            }
+
+            result++;
+            currentMaxIndex = nextMaxIndex;
+            if (currentMaxIndex >= nums.length - 1) {
+                return result;
             }
         }
 
