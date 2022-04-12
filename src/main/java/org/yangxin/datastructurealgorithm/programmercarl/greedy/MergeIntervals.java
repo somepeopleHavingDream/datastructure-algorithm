e23c3516b91c9e04d36214d523c4ab4f848c098f
yangxin <1393003255@qq.com>
2022-04-12 17:59:23 +0800

    feat: MergeIntervals

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/greedy/MergeIntervals.java
@@ -0,0 +1,19 @@
+package org.yangxin.datastructurealgorithm.programmercarl.greedy;
+
+import java.util.Arrays;
+
+/**
+ * @author yangxin
+ * 2022/4/12 11:54
+ */
+public class MergeIntervals {
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        System.out.println(Arrays.deepToString(merge(intervals)));
+    }
+
+    private static int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
