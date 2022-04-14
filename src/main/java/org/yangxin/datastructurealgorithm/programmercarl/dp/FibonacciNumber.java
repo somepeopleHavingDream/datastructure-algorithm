3f55f2ee1138485f9c5fa1a01efffc84d4f6a33b
yangxin <1393003255@qq.com>
2022-04-14 21:35:31 +0800

    feat: FibonacciNumber

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/FibonacciNumber.java
@@ -0,0 +1,32 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/14 21:24
+ */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        System.out.println(fib(2));
+        System.out.println(fib(3));
+        System.out.println(fib(4));
+        System.out.println(fib(0));
+    }
+
+    private static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
