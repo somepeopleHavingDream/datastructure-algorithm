5e251f817d9c0f1063cd18780ce5afaba4ef3178
yangxin <1393003255@qq.com>
2022-04-15 18:07:39 +0800

    feat: UniquePaths

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/UniquePaths.java
@@ -0,0 +1,17 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/15 17:59
+ */
+public class UniquePaths {
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths(3, 2));
+    }
+
+    private static int uniquePaths(int m, int n) {
+        return 0;
+    }
+}
