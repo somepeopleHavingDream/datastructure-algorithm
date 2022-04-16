2643fbe6821a36c4ce821c8a53fc61e9f5e0a675
yangxin <1393003255@qq.com>
2022-04-16 13:55:45 +0800

    feat: UniquePaths

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/UniquePaths.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/UniquePaths.java
@@ -4,6 +4,7 @@ package org.yangxin.datastructurealgorithm.programmercarl.dp;
  * @author yangxin
  * 2022/4/15 17:59
  */
+@SuppressWarnings("SameParameterValue")
 public class UniquePaths {
 
     public static void main(String[] args) {
@@ -12,6 +13,20 @@ public class UniquePaths {
     }
 
     private static int uniquePaths(int m, int n) {
-        return 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
