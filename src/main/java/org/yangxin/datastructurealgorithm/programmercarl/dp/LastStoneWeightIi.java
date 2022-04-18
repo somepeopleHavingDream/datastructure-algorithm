fbc08cb001d7d03a2af517071aca1ffd71eab22a
yangxin <1393003255@qq.com>
2022-04-18 18:06:36 +0800

    feat: LastStoneWeightIi

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/LastStoneWeightIi.java
@@ -0,0 +1,17 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/18 17:13
+ */
+public class LastStoneWeightIi {
+
+    public static void main(String[] args) {
+        int[] stones = {2, 7, 4, 1, 8, 1};
+        System.out.println(lastStoneWeightIi(stones));
+    }
+
+    private static int lastStoneWeightIi(int[] stones) {
+        return 0;
+    }
+}
