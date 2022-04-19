38e927f2b94ff672b819223620d5e1d6e0e2bb49
yangxin <1393003255@qq.com>
2022-04-19 21:50:07 +0800

    feat: ClimbingStairs

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/ClimbingStairs.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/ClimbingStairs.java
@@ -4,12 +4,15 @@ package org.yangxin.datastructurealgorithm.programmercarl.dp;
  * @author yangxin
  * 2022/4/14 21:36
  */
-@SuppressWarnings("AlibabaUndefineMagicConstant")
+@SuppressWarnings({"AlibabaUndefineMagicConstant", "AlibabaRemoveCommentedCode", "SameParameterValue"})
 public class ClimbingStairs {
 
     public static void main(String[] args) {
         System.out.println(climbStairs(2));
         System.out.println(climbStairs(3));
+
+        System.out.println(climbStairs(2, 2));
+        System.out.println(climbStairs(3, 2));
     }
 
     private static int climbStairs(int n) {
@@ -23,6 +26,23 @@ public class ClimbingStairs {
 
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    private static int climbStairs(int n, int m) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (j > i) {
+                    continue;
+                }
+
+                dp[i] += dp[i - j];
+            }
         }
 
         return dp[n];
