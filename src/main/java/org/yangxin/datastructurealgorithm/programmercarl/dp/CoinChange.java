364e2db4e57f266a79465f87ac760ea1ad4ce11b
yangxin <1393003255@qq.com>
2022-04-19 21:53:36 +0800

    feat: CoinChange

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/CoinChange.java
@@ -0,0 +1,18 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/19 21:52
+ */
+@SuppressWarnings({"unused", "SameParameterValue"})
+public class CoinChange {
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        System.out.println(coinChange(coins, 11));
+    }
+
+    private static int coinChange(int[] coins, int amount) {
+        return 0;
+    }
+}
