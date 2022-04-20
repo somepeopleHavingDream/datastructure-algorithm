006fe208d0590c9c6dc5d509e5697f9697420dcf
yangxin <1393003255@qq.com>
2022-04-20 10:21:25 +0800

    feat: CoinChange

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/CoinChange.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/CoinChange.java
@@ -1,5 +1,7 @@
 package org.yangxin.datastructurealgorithm.programmercarl.dp;
 
+import java.util.Arrays;
+
 /**
  * @author yangxin
  * 2022/4/19 21:52
@@ -9,10 +11,31 @@ public class CoinChange {
 
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
+//        int[] coins = {2};
+//        int[] coins = {1};
         System.out.println(coinChange(coins, 11));
     }
 
     private static int coinChange(int[] coins, int amount) {
-        return 0;
+        int[] dp = new int[amount + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int j = 0; j <= amount; j++) {
+                if (coin > j) {
+                    continue;
+                }
+
+                if (dp[j - coin] == Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
