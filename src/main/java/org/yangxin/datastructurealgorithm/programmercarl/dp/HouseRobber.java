bfe25e0ef6bf7d1c287c900df2f5a6a7343d8abb
yangxin <1393003255@qq.com>
2022-04-20 15:47:41 +0800

    feat: HouseRobber

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/HouseRobber.java
@@ -0,0 +1,31 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/20 15:35
+ */
+public class HouseRobber {
+
+    public static void main(String[] args) {
+//        int[] nums = {1, 2, 3, 1};
+//        int[] nums = {2, 7, 9, 3, 1};
+        int[] nums = {0};
+        System.out.println(rob(nums));
+    }
+
+    private static int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1];
+    }
+}
