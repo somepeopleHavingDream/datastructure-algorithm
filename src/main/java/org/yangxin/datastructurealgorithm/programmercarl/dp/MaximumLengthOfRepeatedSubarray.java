71192d3675fe246e6941422794a2684a72204ca4
yangxin <1393003255@qq.com>
2022-04-22 18:16:19 +0800

    feat: MaximumLengthOfRepeatedSubarray

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,17 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/22 14:13
+ */
+public class MaximumLengthOfRepeatedSubarray {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 2, 1}, nums2 = {3, 2, 1, 4, 7};
+        System.out.println(findLength(nums1, nums2));
+    }
+
+    private static int findLength(int[] nums1, int[] nums2) {
+        return 0;
+    }
+}
