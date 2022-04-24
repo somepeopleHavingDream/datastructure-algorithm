abe5ad03c3ba81b5840a3fe1fd6e25d6b95d9157
yangxin <1393003255@qq.com>
2022-04-24 18:09:04 +0800

    feat: LongestCommonSubsequence

---
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/LongestCommonSubsequence.java
@@ -0,0 +1,35 @@
+package org.yangxin.datastructurealgorithm.programmercarl.dp;
+
+/**
+ * @author yangxin
+ * 2022/4/24 17:16
+ */
+public class LongestCommonSubsequence {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubsequence("abcde", "ace"));
+        System.out.println(longestCommonSubsequence("abc", "abc"));
+        System.out.println(longestCommonSubsequence("abc", "def"));
+    }
+
+    private static int longestCommonSubsequence(String text1, String text2) {
+        int length1 = text1.length();
+        int length2 = text2.length();
+        int[][] dp = new int[length1 + 1][length2 + 1];
+
+        int result = 0;
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+
+        return result;
+    }
+}
--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/MaximumLengthOfRepeatedSubarray.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/MaximumLengthOfRepeatedSubarray.java
@@ -7,10 +7,10 @@ package org.yangxin.datastructurealgorithm.programmercarl.dp;
 public class MaximumLengthOfRepeatedSubarray {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 2, 1}, nums2 = {3, 2, 1, 4, 7};
+//        int[] nums1 = {1, 2, 3, 2, 1}, nums2 = {3, 2, 1, 4, 7};
 //        int[] nums1 = {0, 0, 0, 0, 0}, nums2 = {0, 0, 0, 0, 0};
         // 2
-//        int[] nums1 = {0, 1, 1, 1, 1}, nums2 = {1, 0, 1, 0, 1};
+        int[] nums1 = {0, 1, 1, 1, 1}, nums2 = {1, 0, 1, 0, 1};
 //        int[] nums1 = {1, 2, 3, 2, 8}, nums2 = {5, 6, 1, 4, 7};
         System.out.println(findLength(nums1, nums2));
     }
@@ -18,25 +18,12 @@ public class MaximumLengthOfRepeatedSubarray {
     private static int findLength(int[] nums1, int[] nums2) {
         int length1 = nums1.length;
         int length2 = nums2.length;
-        int[][] dp = new int[length1][length2];
+        int[][] dp = new int[length1 + 1][length2 + 1];
 
         int result = 0;
-        for (int i = 0; i < nums1.length; i++) {
-            if (nums1[i] == nums2[0]) {
-                dp[i][0] = 1;
-                result = 1;
-            }
-        }
-        for (int i = 0; i < nums2.length; i++) {
-            if (nums2[i] == nums1[0]) {
-                dp[0][i] = 1;
-                result = 1;
-            }
-        }
-
-        for (int i = 1; i < length1; i++) {
-            for (int j = 1; j < length2; j++) {
-                if (nums1[i] != nums2[j]) {
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (nums1[i - 1] != nums2[j - 1]) {
                     continue;
                 }
 
