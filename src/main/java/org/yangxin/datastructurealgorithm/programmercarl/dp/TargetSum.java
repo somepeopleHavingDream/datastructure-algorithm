adbb1693ee3875eadb71a9c63609be9303f60bc8
yangxin <1393003255@qq.com>
2022-05-19 17:30:31 +0800

    feat: TargetSum

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/TargetSum.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/TargetSum.java
@@ -19,23 +19,23 @@ public class TargetSum {
         if (Math.abs(target) > sum) {
             return 0;
         }
-        if ((target + sum) % 2 == 1) {
+
+        if ((target + sum) % 2 != 0) {
             return 0;
         }
 
-        return dp(nums, (target + sum) / 2);
-    }
+        int bagSize = (target + sum) / 2;
+        int[] dp = new int[bagSize + 1];
 
-    private static int dp(int[] nums, int additiveSum) {
-        int[] dp = new int[additiveSum + 1];
+        // dp[j] += dp[j - nums[i]]
         dp[0] = 1;
 
         for (int num : nums) {
-            for (int j = additiveSum; j >= num; j--) {
+            for (int j = bagSize; j >= num; j--) {
                 dp[j] += dp[j - num];
             }
         }
 
-        return dp[additiveSum];
+        return dp[bagSize];
     }
 }
