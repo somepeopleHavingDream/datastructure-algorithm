803b3fe5113cc05882336f240221f24849cb58be
yangxin <1393003255@qq.com>
2022-05-30 23:09:53 +0800

    feat: MinCostClimbingStairs

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/MinCostClimbingStairs.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/MinCostClimbingStairs.java
@@ -4,6 +4,7 @@ package org.yangxin.datastructurealgorithm.programmercarl.dp;
  * @author yangxin
  * 2022/4/14 21:55
  */
+@SuppressWarnings("ConstantConditions")
 public class MinCostClimbingStairs {
 
     public static void main(String[] args) {
@@ -22,5 +23,22 @@ public class MinCostClimbingStairs {
         }
 
         return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+
+    private static int minCostClimbingStairs2(int[] cost) {
+        // dp[i]: 到达楼层i所需要的最少花费
+        // dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        int[] dp = new int[2];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= cost.length; i++) {
+            int sum = Math.min(dp[0] + cost[i - 1], dp[1] + cost[i - 2]);
+            dp[0] = dp[1];
+            dp[1] = sum;
+        }
+
+        return dp[1];
     }
 }
