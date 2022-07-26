c6e15425d08327e5cb64e18962ff4a40330a291b
yangxin <1393003255@qq.com>
2022-07-26 22:13:03 +0800

    feat: IntegerBreak

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/IntegerBreak.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/IntegerBreak.java
@@ -12,11 +12,17 @@ public class IntegerBreak {
     }
 
     private static int integerBreak(int n) {
+        // dp[i]: 拆分i的所有整数的最大乘积
+        /*
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]))
+            }
+        */
         int[] dp = new int[n + 1];
         dp[2] = 1;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i - j; j++) {
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
                 dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
             }
         }
