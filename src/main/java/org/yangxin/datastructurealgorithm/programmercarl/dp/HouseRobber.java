cc9d6eac8df121285d5b373c18bdfc0abe3d3ffb
yangxin <1393003255@qq.com>
2022-08-01 21:54:43 +0800

    feat: HouseRobber

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/HouseRobber.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/HouseRobber.java
@@ -4,6 +4,7 @@ package org.yangxin.datastructurealgorithm.programmercarl.dp;
  * @author yangxin
  * 2022/4/20 15:35
  */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
 public class HouseRobber {
 
     public static void main(String[] args) {
@@ -18,18 +19,23 @@ public class HouseRobber {
         if (length == 1) {
             return nums[0];
         }
+        if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
 
         // dp[i]: 偷窃到第i间屋子时的最高金额
-        int[] dp = new int[length];
-
         // dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1])
+        int[] dp = new int[3];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < length; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            dp[2] = Math.max(nums[i] + dp[0], dp[1]);
+
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
 
-        return dp[length - 1];
+        return dp[2];
     }
 }
