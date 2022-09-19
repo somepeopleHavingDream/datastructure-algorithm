504487714425e501d35153d0ed04f90dc0d77be7
yangxin <1393003255@qq.com>
2022-09-19 17:32:28 +0800

    feat: BestTimeToBuyAndSellStockIii

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIii.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIii.java
@@ -33,20 +33,20 @@ public class BestTimeToBuyAndSellStockIii {
             return 0;
         }
 
-        int[][] dp = new int[1][5];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        dp[0][2] = 0;
-        dp[0][3] = -prices[0];
-        dp[0][4] = 0;
+        int[] dp = new int[5];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+        dp[2] = 0;
+        dp[3] = -prices[0];
+        dp[4] = 0;
 
         for (int i = 1; i < prices.length; i++) {
-            dp[0][1] = Math.max(dp[0][0] - prices[i], dp[0][1]);
-            dp[0][2] = Math.max(dp[0][1] + prices[i], dp[0][2]);
-            dp[0][3] = Math.max(dp[0][2] - prices[i], dp[0][3]);
-            dp[0][4] = Math.max(dp[0][3] + prices[i], dp[0][4]);
+            dp[1] = Math.max(dp[0] - prices[i], dp[1]);
+            dp[2] = Math.max(dp[1] + prices[i], dp[2]);
+            dp[3] = Math.max(dp[2] - prices[i], dp[3]);
+            dp[4] = Math.max(dp[3] + prices[i], dp[4]);
         }
 
-        return Math.max(dp[0][2], dp[0][4]);
+        return Math.max(dp[2], dp[4]);
     }
 }
