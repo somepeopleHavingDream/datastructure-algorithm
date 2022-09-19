bc43c71c45f8c319fdc6cf5a9b32ea9e5a1b5bbc
yangxin <1393003255@qq.com>
2022-09-19 17:25:15 +0800

    feat: BestTimeToBuyAndSellStock

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStock.java
@@ -50,16 +50,15 @@ public class BestTimeToBuyAndSellStock {
             return 0;
         }
 
-        int[][] dp = new int[1][2];
+        int[] dp = new int[2];
 
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+        dp[1] = -prices[0];
 
         for (int i = 1; i < prices.length; i++) {
-            dp[0][0] = Math.max(dp[0][0], dp[0][1] + prices[i]);
-            dp[0][1] = Math.max(dp[0][1], -prices[i]);
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], -prices[i]);
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
