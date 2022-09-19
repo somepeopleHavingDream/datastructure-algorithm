dc530984b164ee5826af8f49497bf2822885a1b1
yangxin <1393003255@qq.com>
2022-09-19 09:57:08 +0800

    feat: BestTimeToBuyAndSellStock

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStock.java
@@ -1,9 +1,12 @@
 package org.yangxin.datastructurealgorithm.programmercarl.dp;
 
+import java.util.Objects;
+
 /**
  * @author yangxin
  * 2022/4/20 16:42
  */
+@SuppressWarnings({"AlibabaRemoveCommentedCode", "DeprecatedIsStillUsed"})
 public class BestTimeToBuyAndSellStock {
 
     public static void main(String[] args) {
@@ -19,26 +22,44 @@ public class BestTimeToBuyAndSellStock {
      */
 
     private static int maxProfitByGreedy(int[] prices) {
-        int low = Integer.MAX_VALUE;
+        // 用贪心法
+        int left = Integer.MAX_VALUE;
         int result = 0;
         for (int price : prices) {
-            low = Math.min(low, price);
-            result = Math.max(result, price - low);
+            left = Math.min(left, price);
+            result = Math.max(result, price - left);
         }
 
         return result;
     }
 
+    /**
+     * 优先贪心，不建议滚动数组
+     *
+     * @param prices 股票当日价格
+     * @return 最大收益
+     */
+    @Deprecated
     private static int maxProfitByDp(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        // 用动态规划法（滚动数组）
+        // dp[i][0]：第i天未持有股票所获得的最大收益
+        // dp[i][1]：第i天持有股票所获得的最大收益
+        // dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+        // dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        if (Objects.isNull(prices)) {
+            return 0;
         }
 
-        return dp[prices.length - 1][1];
+        int[][] dp = new int[1][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[0][0] = Math.max(dp[0][0], dp[0][1] + prices[i]);
+            dp[0][1] = Math.max(dp[0][1], -prices[i]);
+        }
+
+        return dp[0][0];
     }
 }
--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIi.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIi.java
@@ -15,7 +15,7 @@ public class BestTimeToBuyAndSellStockIi {
     }
 
     /*
-        显然，贪心算法的性能比动态规划的好
+        显然，贪心算法的性能比动态规划的好，但不建议用贪心，因为贪心的思路较难想出来，而动规很容易查出来
      */
 
     private static int maxProfitByGreedy(int[] prices) {
@@ -30,15 +30,24 @@ public class BestTimeToBuyAndSellStockIi {
     }
 
     private static int maxProfitByDp(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
+        // dp[i][0]: 第i天不持有股票，所能获取到的最大利润
+        // dp[i][1]：第i天持有股票，所能获取到的最大利润
 
-        for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        // dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+        // dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+        if (prices == null) {
+            return 0;
         }
 
-        return dp[prices.length - 1][1];
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[length - 1][0];
     }
 }
