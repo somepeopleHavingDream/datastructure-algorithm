537dd34c475cb3af2fe6999ad839f31099e8a0c4
yangxin <1393003255@qq.com>
2022-09-19 10:57:18 +0800

    feat: BestTimeToBuyAndSellStockIii

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIii.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockIii.java
@@ -1,9 +1,12 @@
 package org.yangxin.datastructurealgorithm.programmercarl.dp;
 
+import java.util.Objects;
+
 /**
  * @author yangxin
  * 2022/4/20 16:42
  */
+@SuppressWarnings({"AlibabaRemoveCommentedCode", "CommentedOutCode"})
 public class BestTimeToBuyAndSellStockIii {
 
     public static void main(String[] args) {
@@ -14,7 +17,23 @@ public class BestTimeToBuyAndSellStockIii {
     }
 
     private static int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][5];
+        // dp[i][0]：在第i天初次不持有股票的最大现金量
+        // dp[i][1]：在第i天第1次持有股票的最大现金量
+        // dp[i][2]：在第i天第1次不持有股票的最大现金量
+        // dp[i][3]：在第i天第2次持有股票的最大现金量
+        // dp[i][4]：在第i天第2次不持有股票的最大现金量
+
+        // dp[i][0] = dp[i - 1][0];
+        // dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+        // dp[i][2] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][2]);
+        // dp[i][3] = Math.max(dp[i - 1][2] - prices[i], dp[i - 1][3]);
+        // dp[i][4] = Math.max(dp[i - 1][3] + prices[i], dp[i - 1][4]);
+
+        if (Objects.isNull(prices)) {
+            return 0;
+        }
+
+        int[][] dp = new int[1][5];
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         dp[0][2] = 0;
@@ -22,13 +41,12 @@ public class BestTimeToBuyAndSellStockIii {
         dp[0][4] = 0;
 
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = dp[i - 1][0];
-            dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
-            dp[i][2] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][2]);
-            dp[i][3] = Math.max(dp[i - 1][2] - prices[i], dp[i - 1][3]);
-            dp[i][4] = Math.max(dp[i - 1][3] + prices[i], dp[i - 1][4]);
+            dp[0][1] = Math.max(dp[0][0] - prices[i], dp[0][1]);
+            dp[0][2] = Math.max(dp[0][1] + prices[i], dp[0][2]);
+            dp[0][3] = Math.max(dp[0][2] - prices[i], dp[0][3]);
+            dp[0][4] = Math.max(dp[0][3] + prices[i], dp[0][4]);
         }
 
-        return dp[prices.length - 1][4];
+        return Math.max(dp[0][2], dp[0][4]);
     }
 }
