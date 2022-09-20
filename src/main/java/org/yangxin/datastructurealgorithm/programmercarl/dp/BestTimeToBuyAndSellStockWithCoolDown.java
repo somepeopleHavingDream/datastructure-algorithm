203d9d049c70a1dcfaeda13834e8e2fb40b0ccca
yangxin <1393003255@qq.com>
2022-09-20 13:38:19 +0800

    feat: BestTimeToBuyAndSellStockWithCoolDown

--- src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockWithCoolDown.java
+++ src/main/java/org/yangxin/datastructurealgorithm/programmercarl/dp/BestTimeToBuyAndSellStockWithCoolDown.java
@@ -1,9 +1,12 @@
 package org.yangxin.datastructurealgorithm.programmercarl.dp;
 
+import java.util.Objects;
+
 /**
  * @author yangxin
  * 2022/4/20 16:42
  */
+@SuppressWarnings({"CommentedOutCode", "AlibabaRemoveCommentedCode"})
 public class BestTimeToBuyAndSellStockWithCoolDown {
 
     public static void main(String[] args) {
@@ -16,37 +19,29 @@ public class BestTimeToBuyAndSellStockWithCoolDown {
     }
 
     private static int maxProfit(int[] prices) {
-        if (prices.length <= 0) {
+        // dp[i][0]：第i天持有股票的最大金额量
+        // dp[i][1]：第i天不持有股票（当天卖掉股票）的最大金额量
+        // dp[i][2]：第i天不持有股票（非当天卖掉股票）的最大金额量
+
+        // dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+        // dp[i][1] = dp[i - 1][0] + prices[i];
+        // dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1]);
+
+        if (Objects.isNull(prices)) {
             return 0;
         }
 
-        int[][] dp = new int[prices.length][3];
-
-        /*
-            卖出股票后，才有冷冻期
-         */
-
-        // 第0天，持有股票
-        dp[0][0] = -prices[0];
-        // 第0天，不持有股票，但并不是今天卖出的
-        dp[0][1] = 0;
-        // 第0天，不持有股票，因为今天卖出股票
-        dp[0][2] = 0;
+        int[] dp = new int[3];
+        dp[0] = -prices[0];
 
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][2] = dp[i - 1][0] + prices[i];
+            int oldDp0 = dp[0];
+            int oldDp1 = dp[1];
+            dp[0] = Math.max(dp[0], dp[2] - prices[i]);
+            dp[1] = oldDp0 + prices[i];
+            dp[2] = Math.max(dp[2], oldDp1);
         }
 
-        return max(dp[prices.length - 1][1], dp[prices.length - 1][2]);
-    }
-
-    private static int max(int... array) {
-        int max = Integer.MIN_VALUE;
-        for (int i : array) {
-            max = Math.max(max, i);
-        }
-        return max;
+        return Math.max(dp[1], dp[2]);
     }
 }
